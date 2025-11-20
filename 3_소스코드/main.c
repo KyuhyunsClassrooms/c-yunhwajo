698721feab7c1bdfe8d20892e4d5742ef995a331
yunhwajo <120474207+yunhwajo@users.noreply.github.com>
2025-11-20 05:25:05 +0000

    최종 제출

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,78 +1,42 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-void clearBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
-
-struct CircuitInput {
-    bool A;
-    bool B;
-    bool C;
-};
-
-struct CircuitOutput {
-    bool X;
-    bool Y;
-};
-
-struct CircuitInput getValidInput() {
-    struct CircuitInput input;
-    int a, b, c;
-
-    while (1) {
-        printf("A, B, C 입력 (0 또는 1): ");
-        scanf("%d %d %d", &a, &b, &c);
-
-        if ((a == 0 || a == 1) &&
-            (b == 0 || b == 1) &&
-            (c == 0 || c == 1)) {
-
-            input.A = a;
-            input.B = b;
-            input.C = c;
-            clearBuffer();
-            return input;
-        }
-
-        printf("잘못된 입력입니다. A, B, C는 0 또는 1이어야 합니다.\n");
-        clearBuffer();
-    }
-}
-
-struct CircuitOutput compute(struct CircuitInput in) {
-    struct CircuitOutput out;
-
-    bool X1 = in.A && in.B;
-    bool X2 = (!in.B) && in.C;
-    out.X = X1 || X2;
-
-    bool Y1 = in.A || in.C;
-    out.Y = Y1 && (!in.B);
-
-    return out;
-}
-
-void printOutput(struct CircuitOutput out) {
-    printf("X = %d\n", out.X);
-    printf("Y = %d\n", out.Y);
-}
 
 int main() {
+    int A, B, C;
     char choice;
 
+    printf("🎔 논리 회로 시뮬레이터 🎔\n");
+    printf("이 프로그램은 A B C의 세 가지 값을 입력받아\n");
+    printf("논리 연산을 이용해 두 개의 출력 X와 Y를 계산합니다.\n\n");
+
+    printf("<사용 방법>\n");
+    printf("A, B, C에 0 또는 1을 입력하세요!\n");
+    printf("다음 논리식에 따라 X와 Y가 계산됩니다:\n");
+    printf("  X = (A AND B) OR (NOT B AND C)\n");
+    printf("  Y = (A OR C) AND (NOT B)\n");
+    printf("반드시 띄어쓰기를 지켜주세요!\n\n");
+
     do {
-        struct CircuitInput input = getValidInput();
-        struct CircuitOutput output = compute(input);
-        printOutput(output);
+        printf("A B C 입력 (각각 0 또는 1): ");
+        scanf("%d %d %d", &A, &B, &C);
+
+        if ((A != 0 && A != 1) || (B != 0 && B != 1) || (C != 0 && C != 1)) {
+            printf("잘못된 입력입니다! 0 또는 1만 입력해주세요.\n\n");
+            continue;
+        }
+
+        int X = (A && B) || ((!B) && C);
+        int Y = (A || C) && (!B);
+
+        printf("\n=== 결과 ===\n");
+        printf("X = %d\n", X);
+        printf("Y = %d\n\n", Y);
 
         printf("다시 실행할까요? (y/n): ");
         scanf(" %c", &choice);
-        clearBuffer();
+        printf("\n");
 
     } while (choice == 'y' || choice == 'Y');
 
+    printf("프로그램을 종료합니다.\n");
     return 0;
 }
-
